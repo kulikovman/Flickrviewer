4e1186774b955b2a6bc9f0e458baf5bb9c8e7224
Maksim Kulikov <kulikovman@gmail.com>
2018-06-13 19:55:40 +0500

    Парсинг json ответа и формирования списка фотографий

--- app/src/main/java/ru/kulikovman/flickrviewer/models/GalleryItem.java
+++ app/src/main/java/ru/kulikovman/flickrviewer/models/GalleryItem.java
@@ -5,6 +5,30 @@ public class GalleryItem {
     private String mId;
     private String mUrl;
 
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
     @Override
     public String toString() {
         return mCaption;
