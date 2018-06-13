96423707c0ffb04f7e7e8a190a955b6f21c7ec72
Maksim Kulikov <kulikovman@gmail.com>
2018-06-13 19:24:08 +0500

    Внутренний AsyncTask класс

--- app/src/main/java/ru/kulikovman/flickrviewer/PhotoGalleryActivity.java
+++ app/src/main/java/ru/kulikovman/flickrviewer/PhotoGalleryActivity.java
@@ -1,13 +1,19 @@
 package ru.kulikovman.flickrviewer;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.io.IOException;
 
 import ru.kulikovman.flickrviewer.adapters.PhotoAdapter;
 
 public class PhotoGalleryActivity extends AppCompatActivity {
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mRecyclerView;
     private PhotoAdapter mAdapter;
 
@@ -27,5 +33,19 @@ public class PhotoGalleryActivity extends AppCompatActivity {
         mAdapter = new PhotoAdapter();
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr()
+                        .getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
