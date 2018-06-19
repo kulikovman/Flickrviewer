57938e06fb341d30b3cd18190cd27a60a445a308
Maksim Kulikov <kulikovman@gmail.com>
2018-06-20 01:38:20 +0500

    На карту добавлены кнопки масштаба и текущего местоположения

--- app/src/main/java/ru/kulikovman/flickrviewer/MapsActivity.java
+++ app/src/main/java/ru/kulikovman/flickrviewer/MapsActivity.java
@@ -1,19 +1,26 @@
 package ru.kulikovman.flickrviewer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private UiSettings mUiSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,30 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mUiSettings = mMap.getUiSettings();
+
+        // Keep the UI Settings state in sync with the checkboxes.
+        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setMyLocationButtonEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        /*mUiSettings.setCompassEnabled(isChecked(R.id.compass_toggle));
+        mUiSettings.setMyLocationButtonEnabled(isChecked(R.id.mylocationbutton_toggle));
+        mMap.setMyLocationEnabled(isChecked(R.id.mylocationlayer_toggle));
+        mUiSettings.setScrollGesturesEnabled(isChecked(R.id.scroll_toggle));
+        mUiSettings.setZoomGesturesEnabled(isChecked(R.id.zoom_gestures_toggle));
+        mUiSettings.setTiltGesturesEnabled(isChecked(R.id.tilt_toggle));
+        mUiSettings.setRotateGesturesEnabled(isChecked(R.id.rotate_toggle));*/
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
